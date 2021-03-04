653be93d934da4ab7312ba4d315da642af58b916
Giovanny <giov_w@hotmail.com>
2021-03-04 14:35:15 -0500

    Initial Sommet Front

---
+++ shopware-pwa.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  shopwareEndpoint: "http://localhost:8888/sommet-sw-back/public",
+  shopwareAccessToken: "SWSCDKHYRWV6UNZHT2ZXTLHWDW",
+  theme: "sommet-theme",
+  pwaHost: "https://pwa.swstage.store",
+};
